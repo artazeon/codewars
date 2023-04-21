c4fe746dc985b06c9a5d02d99bb0d9445228247b
artazeon <artazeon@gmail.com>
2023-04-21 09:21:23 +0300

    3 kata

---
+++ 5 without numbers !! copy.js
@@ -0,0 +1,7 @@
+function unusualFive() {
+  const arr = ["a", "b", "c", "d", "f"]
+  return arr.length
+}
+
+
+console.log(unusualFive())
---
+++ Invert values.js
@@ -0,0 +1,5 @@
+function invert(array) {
+  return array.map((num) => num* -1)
+}
+
+console.log(invert([1,2,3,4,5]))
