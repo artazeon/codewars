9fa2d3843e37ed2ecfd79b48b1df46848d860258
artazeon <artazeon@gmail.com>
2023-05-04 13:34:04 +0300

    Return Negative

--- 1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function flattenAndSort(array) {
-  const arrN = array.map((i)=> i.split(','))
-  const srt = arrN.join(',')
-  return srt;
-}
-
-console.log([[3, 2, 1], [7, 9, 8], [6, 4, 5]])
