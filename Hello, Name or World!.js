96fbf4efbdbc49ce9eb387d05b685f09daf49fb3
artazeon <artazeon@gmail.com>
2023-05-31 13:21:38 +0300

    8ky

---
+++ Hello, Name or World!.js
@@ -0,0 +1,14 @@
+function hello(name = 'World') {
+
+  if (name === '') {
+    name = 'World'
+  }
+
+  const newName = name.charAt(0).toUpperCase() + name.toLowerCase().slice(1);
+  
+  return `Hello, ${newName}!`;
+}
+
+console.log(hello('alice'))
+console.log(hello())
+console.log(hello(''))
