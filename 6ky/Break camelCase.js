00937f8cec233357bfab6abdc03aee73c212942a
artazeon <artazeon@gmail.com>
2023-07-01 12:56:37 +0300

    6ky

---
+++ 6ky/Break camelCase.js
@@ -0,0 +1,14 @@
+function solution(string) {
+  const arr = []
+
+  for (let i = 0; i < string.length; i++) {
+    
+    if ( string.charAt(i) === string.charAt(i).toUpperCase() ) {
+      arr.push(` ${string.charAt(i)}`)
+    }
+    else {arr.push(`${string.charAt(i)}`)}
+}
+  return arr.join('')
+}
+console.log(solution('camelCasing'))
+console.log(solution('camelCasingTest'))
