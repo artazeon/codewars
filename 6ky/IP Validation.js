46d493e6000765e4b26945bae9bd6d07aaa6e5ae
artazeon <artazeon@gmail.com>
2023-07-12 14:52:37 +0300

    6ky

---
+++ 6ky/IP Validation.js
@@ -0,0 +1,6 @@
+function isValidIP(str) {
+  const pattern = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/;
+  return pattern.test(str);
+}
+
+console.log(isValidIP("12.255.56.1"))
