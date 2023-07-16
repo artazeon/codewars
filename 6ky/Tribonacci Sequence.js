2f6a67b2b3fb0c4df4a07516b047115a16abeff8
artazeon <artazeon@gmail.com>
2023-07-16 13:16:08 +0300

    6ky

---
+++ 6ky/Tribonacci Sequence.js
@@ -0,0 +1,20 @@
+function tribonacci(signature, n) {
+  let arr = []
+  if (n >= 3) {
+    arr = [...signature]
+    for (let i = 0; i < n - 3; i++) {
+      arr.push(arr[arr.length - 3] + arr[arr.length - 2] + arr[arr.length - 1])
+    }
+  } else if (n < 3) {
+
+    for (let i = 0; i < n; i++) {
+      arr.push(signature[i])
+    }
+
+  }
+
+
+  return arr
+}
+
+console.log(tribonacci([1, 1, 1], 1))
