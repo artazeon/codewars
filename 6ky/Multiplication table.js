bf080289dd093aed7e74f9f7a3ffcfb5e715a4a2
artazeon <artazeon@gmail.com>
2023-08-17 21:02:44 +0300

    6ky

---
+++ 6ky/Multiplication table.js
@@ -0,0 +1,21 @@
+multiplicationTable = function (size) {
+  let table = []
+
+  for (let i = 1; i <= size; i++) {
+    let row = []
+
+    
+
+    for (let k = 1; k <= size; k++) {
+      row.push(i * k)
+    }
+
+    table.push(row)
+
+
+  }
+
+  return table
+}
+
+console.log(multiplicationTable(3))
