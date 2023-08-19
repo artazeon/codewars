245cbc6291405884513f53917d6aa1a095dd5072
artazeon <artazeon@gmail.com>
2023-08-19 20:31:08 +0300

    6ky

--- 6ky/Multiplication table.js
+++ 6ky/Multiplication table.js
@@ -4,15 +4,11 @@ multiplicationTable = function (size) {
   for (let i = 1; i <= size; i++) {
     let row = []
 
-    
-
     for (let k = 1; k <= size; k++) {
       row.push(i * k)
     }
 
     table.push(row)
-
-
   }
 
   return table
