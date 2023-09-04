8a96d190c34162e7bdad1c447e35e1b12239f692
artazeon <artazeon@gmail.com>
2023-09-04 11:00:29 +0300

    8ky

--- 8ky/5 without numbers !! copy.js
+++
@@ -1,6 +0,0 @@
-function unusualFive() {
-  const arr = ['a', 'b', 'c', 'd', 'f']
-  return arr.length
-}
-
-console.log(unusualFive())
---
+++ 8ky/Welcome to the City.js
@@ -0,0 +1,7 @@
+function sayHello( name, city, state ) {
+  const allName = name.join(' ')
+
+  return `Hello, ${allName}! Welcome to ${city}, ${state}!`
+}
+
+console.log(sayHello(['John', 'Smith'], 'Phoenix', 'Arizona'))
