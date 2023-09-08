e84283f45870e5800867f775916cc87c9f92013d
artazeon <artazeon@gmail.com>
2023-09-08 16:31:19 +0300

    7ky

---
+++ 7ky/Mumbling.js
@@ -0,0 +1,12 @@
+function accum(s) {
+	const arr = s.toLowerCase().split('')
+
+  const accArr = arr.map((el, index) => {
+    return el.toUpperCase() + el.repeat(index)
+  })
+
+  return accArr.join('-')
+}
+
+
+console.log(accum("ZpglnRxqenU"))
