6748bd679b83805a4708c56acdfb6829d76d738e
artazeon <artazeon@gmail.com>
2023-09-12 20:20:12 +0300

    7ky

---
+++ 7ky/Beginner Series #3 Sum of Numbers copy.js
@@ -0,0 +1,9 @@
+function friend(friends){
+  const realFriends = friends.filter(el => {
+    return el.length === 4
+  })
+
+  return realFriends
+}
+
+console.log(friend(["Ryan", "Kieran", "Mark"]))
