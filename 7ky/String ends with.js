d8bdf84ae41af2f43a527e1a2015283edf788bd3
artazeon <artazeon@gmail.com>
2023-09-12 13:29:56 +0300

    7ky

---
+++ 7ky/String ends with.js
@@ -0,0 +1,10 @@
+function solution(str, ending){
+  const endStr = str.slice(str.length - ending.length, str.length)  
+
+  
+   return endStr === ending
+}
+
+console.log(solution('abcde', 'cde'))
+console.log(solution('abcde', 'abc'))
+console.log(solution('samurai', 'ai'))
