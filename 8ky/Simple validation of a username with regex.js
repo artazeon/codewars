9b7bf65c017e192a70f1901305fe570c705dc72a
artazeon <artazeon@gmail.com>
2023-09-23 21:42:24 +0300

    8ky

--- 8ky/Simple validation of a username with regex.js
+++ 8ky/Simple validation of a username with regex.js
@@ -1,35 +1,4 @@
-function comp(array1, array2) {
-
-  if (array1 == null || array2 == null ) {
-    return false
-  }
-
-  const arr1 = array1.sort((a, b) => a - b)
-  const arr2 = array2.sort((a, b) => a - b)
-
-  const res = arr1.map((number, index) => {
-    return Math.pow(number, 2) === arr2[index];
-  });
-
-
-  return !res.includes(false)
+function validateUsr(username) {
+  res =  /^[a-z0-9_]{4,16}$/.test(username) 
+  return res
 }
-
-console.log(
-  comp(
-    [121, 144, 19, 161, 19, 144, 19, 11],
-    [
-      11 * 11,
-      121 * 121,
-      144 * 144,
-      19 * 19,
-      161 * 161,
-      19 * 19,
-      144 * 144,
-      19 * 19,
-    ]
-  )
-)
-console.log(comp([2, 2, 3], [4, 9, 9]))
-console.log(comp([], []))
-console.log(comp(null, []))
