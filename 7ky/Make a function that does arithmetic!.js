1f303d71430ad3c03ce46005485c9205ac2acd22
artazeon <artazeon@gmail.com>
2023-10-03 13:33:50 +0300

    7ky

---
+++ 7ky/Make a function that does arithmetic!.js
@@ -0,0 +1,26 @@
+function arithmetic(a, b, operator) {
+  let res = 0
+  switch (operator) {
+    case 'add':
+      res = a + b
+      break
+    case 'subtract':
+      res = a - b
+      break
+
+    case 'multiply':
+      res = a * b
+
+      break
+      case 'divide':
+      res = a / b
+      break
+  }
+
+  return res
+}
+
+console.log(arithmetic(1, 2, 'add'))
+console.log(arithmetic(8, 2, 'subtract'))
+console.log(arithmetic(5, 2, 'multiply'))
+console.log(arithmetic(8, 2, 'divide'))
