808381936e1af7387affbb9d0d1d921dd55284ed
artazeon <artazeon@gmail.com>
2023-10-25 08:51:56 +0300

    7ky

--- 7ky/Fix string case.js
+++ 7ky/Fix string case.js
@@ -1,20 +1,16 @@
-function solve(s){
+function solve(s) {
   let lowerCase = 0
   let upperCase = 0
 
-  for (let i=0; i<s.length; i++) {
-    if (s[i]===s[i].toUpperCase()) {
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i].toUpperCase()) {
       upperCase++
-    }
-
-    else {
+    } else {
       lowerCase++
     }
   }
 
-
-
   return upperCase > lowerCase ? s.toUpperCase() : s.toLowerCase()
 }
 
-console.log(solve("CODe"))
+console.log(solve('CODe'))
