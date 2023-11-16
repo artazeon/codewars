0a6fc7da479f7dede1b66e0765c5116a792616d5
artazeon <artazeon@gmail.com>
2023-11-16 13:56:35 +0300

    7ky

---
+++ 7ky/Love vs friendship.js
@@ -0,0 +1,40 @@
+function wordsToMarks(string) {
+  const arrString = string.split('')
+  const alphabet = [
+    0,
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ]
+
+  const summ = arrString.reduce((ac, el, ind) => {
+    return ac + alphabet.indexOf(el)
+  }, 0)
+
+  return summ
+}
+
+console.log(wordsToMarks('attitude'))
