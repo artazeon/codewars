57fc9de9b7a41bea8f7a78b226b57c297b3a0beb
artazeon <artazeon@gmail.com>
2023-12-02 15:50:58 +0300

    7ky

---
+++ 7ky/Unlucky Days.js
@@ -0,0 +1,14 @@
+function unluckyDays(year) {
+  let count = 0
+
+  for (let month = 0; month < 12; month++) {
+    const date = new Date(year, month, 13)
+    if (date.getDay() === 5) {
+      count++
+    }
+  }
+
+  return count
+}
+
+console.log(unluckyDays(1001))
