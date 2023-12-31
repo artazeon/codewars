f0d5f2821c141d3de4129bf36d6f9b3e6b94c726
artazeon <artazeon@gmail.com>
2023-12-31 14:59:41 +0300

    7ky

---
+++ 7ky/Alphabet symmetry.js
@@ -0,0 +1,25 @@
+function solve(arr){  
+  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+  const res = arr.map(el => {
+    let count = 0
+    el = el.toLowerCase()
+    console.log(el)
+
+    for (let i = 0; i <= el.length; i++) {
+      if (el[i] === alphabet[i]) {
+        count++
+      }
+    }
+
+    return count
+  })
+
+  return res
+
+
+  };
+
+
+
+console.log(solve(["abode","ABc","xyzD"]))
