2cba4b28bd5395795736b545ba4a249977fcc502
artazeon <artazeon@gmail.com>
2024-01-16 10:24:01 +0300

    7ky

---
+++ 7ky/Coding Meetup #3 - Higher-Order Functions Series - Is Ruby coming.js
@@ -0,0 +1,32 @@
+const list = [
+  {
+    firstName: 'Sofia',
+    lastName: 'I.',
+    country: 'Argentina',
+    continent: 'Americas',
+    age: 35,
+    language: 'Java',
+  },
+  {
+    firstName: 'Lukas',
+    lastName: 'X.',
+    country: 'Croatia',
+    continent: 'Europe',
+    age: 35,
+    language: 'Python',
+  },
+  {
+    firstName: 'Madison',
+    lastName: 'U.',
+    country: 'United States',
+    continent: 'Americas',
+    age: 32,
+    language: 'Ruby',
+  },
+]
+
+function greetDevelopers(list) {
+  return list.some((el) => el.language === 'Ruby')
+}
+
+console.log(greetDevelopers(list))
