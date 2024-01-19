959fccfa61fcb4cb7e618efafc289f490139229c
artazeon <artazeon@gmail.com>
2024-01-19 15:12:15 +0300

    7ky

---
+++ 7ky/Coding Meetup #6 - Higher-Order Functions Series - Can they code in the same language.js
@@ -0,0 +1,21 @@
+var list1 = [
+  { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+  { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+];
+
+var list2 = [
+  { firstName: 'Mariami', lastName: 'G.', country: 'Georgia', continent: 'Europe', age: 29, language: 'Python' },
+  { firstName: 'Mia', lastName: 'H.', country: 'Germany', continent: 'Europe', age: 39, language: 'Ruby' },
+  { firstName: 'Maria', lastName: 'I.', country: 'Greece', continent: 'Europe', age: 32, language: 'C' },
+];
+
+function isSameLanguage(list) {
+  const first = list[0].language
+  const res = list.every((el) => el.language === first)
+  return res
+}
+
+console.log(isSameLanguage(list1))
+console.log(isSameLanguage(list2))
+
