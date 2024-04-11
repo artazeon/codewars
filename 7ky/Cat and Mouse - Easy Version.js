f58714c5caf35f02462e4dc86bd2d461c524a1c8
artazeon <artazeon@gmail.com>
2024-04-11 15:06:36 +0300

    7ky

--- 7ky/Cat and Mouse - Easy Version.js
+++ 7ky/Cat and Mouse - Easy Version.js
@@ -1,7 +1,7 @@
 function catMouse(x) {
-  const length = x.slice(1, -1)
+  const jump = x.slice(1, -1)
 
-  return length.length > 3 ? 'Escaped!' : 'Caught!'
+  return jump.length > 3 ? 'Escaped!' : 'Caught!'
 }
 console.log(catMouse('C....m'))
 console.log(catMouse('C..m'))
