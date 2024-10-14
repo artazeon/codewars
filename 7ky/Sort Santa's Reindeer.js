a0fc3e52d3dbacd2674131b92b56654ad073b1b5
artazeon <artazeon@gmail.com>
2024-10-14 16:46:24 +0300

    7ky

---
+++ 7ky/Sort Santa's Reindeer.js
@@ -0,0 +1,15 @@
+const sortReindeer = (reindeer) =>
+  reindeer.sort((a, b) => a.split(' ')[1].localeCompare(b.split(' ')[1]))
+
+console.log(
+  sortReindeer([
+    'Dasher Tonoyan',
+    'Dancer Moore',
+    'Prancer Chua',
+    'Vixen Hall',
+    'Comet Karavani',
+    'Cupid Foroutan',
+    'Donder Jonker',
+    'Blitzen Claus',
+  ])
+)
