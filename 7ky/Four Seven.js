2acf0147215f9790a49826c266cc4898eb4d8421
artazeon <artazeon@gmail.com>
2024-12-19 11:36:02 +0300

    7ky

---
+++ 7ky/Four Seven.js
@@ -0,0 +1,12 @@
+function fourSeven(n) {
+  while (n == 4) {
+    return 7
+  }
+  while (n == 7) {
+    return 4
+  }
+  return 0
+}
+console.log(fourSeven(4))
+console.log(fourSeven(7))
+console.log(fourSeven(79))
