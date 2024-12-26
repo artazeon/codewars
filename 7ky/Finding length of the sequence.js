7ad451a8ea5eb04148dda623aedc511355813229
artazeon <artazeon@gmail.com>
2024-12-26 12:51:04 +0300

    7ky

---
+++ 7ky/Finding length of the sequence.js
@@ -0,0 +1,11 @@
+const lengthOfSequence = (arr, n) => {
+  const newArr = arr.join('').split(n)
+
+  if (newArr.length !== 3) return 0
+
+  const res = newArr[1].replace(/-/g, '')
+  return res.length + 2
+}
+
+console.log(lengthOfSequence([0, -3, 7, 4, 0, 3, 7, 9], 7))
+console.log(lengthOfSequence([0, 8, -7, 6, 1, 2, -7, 4, 8, 9], 8))
