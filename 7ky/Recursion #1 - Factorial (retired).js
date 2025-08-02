c6ba9563c016c6bafb1e43f33fa276627bce0dff
artazeon <artazeon@gmail.com>
2025-08-02 12:39:38 +0300

    7ky

---
+++ 7ky/Recursion #1 - Factorial (retired).js
@@ -0,0 +1,8 @@
+const factorial = (n) => (n === 0 || n === 1 ? 1 : n * factorial(n - 1))
+
+console.log(factorial(0))
+console.log(factorial(1))
+console.log(factorial(2))
+console.log(factorial(3))
+console.log(factorial(4))
+console.log(factorial(5))
