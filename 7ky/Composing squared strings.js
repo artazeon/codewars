6cb0b8d63dfb0a88f72d0db865bc6e3487ee5e9a
artazeon <artazeon@gmail.com>
2025-10-06 12:04:44 +0300

    7ky

---
+++ 7ky/Composing squared strings.js
@@ -0,0 +1,12 @@
+function compose(s1, s2) {
+  const arr1 = s1.split('\n')
+  const arr2 = s2.split('\n').reverse()
+
+  const res = arr1.map((el, ind) => {
+    return el.slice(0, ind + 1) + arr2[ind].slice(0, el.length - ind)
+  })
+
+  return res
+}
+
+console.log(compose('abcd\nefgh\nijkl\nmnop', 'qrst\nuvwx\nyz12\n3456'))
