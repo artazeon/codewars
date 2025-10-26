def7c6ff4ff34048eb9e6cfb69b8a031cc0d9160
artazeon <artazeon@gmail.com>
2025-10-26 12:48:24 +0300

    7ky

---
+++ 7ky/United State of React.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export class States extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      united: false,
+    }
+  }
+  unite() {
+    this.setState({ united: true })
+  }
+
+  render() {
+    return (
+      <div className="status">
+        {this.state.united ? 'Code for everyone' : 'Make America code again'}
+      </div>
+    )
+  }
+}
