828ba7c58eb8b25752e798065e285b29d055b170
artazeon <artazeon@gmail.com>
2026-01-09 16:14:54 +0300

    7ky

---
+++ 7ky/16+18=214 copy.js
@@ -0,0 +1,8 @@
+function removeBMW(str) {
+  if (typeof str !== 'string') {
+    throw new Error('This program only works for text.')
+  }
+  return str.replace(/[bmw]/gi, '')
+}
+
+console.log(removeBMW('bmwvolvoBMW'))
