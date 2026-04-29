b76ccd36f2967aa970d4fdd5da6bdb00a3fbd734
artazeon <artazeon@gmail.com>
2026-04-29 13:11:35 +0300

    7ky

---
+++ 7ky/yu Check whether a number is valid in a given numeral system.js
@@ -0,0 +1,46 @@
+function validateBase(num, base) {
+  const digits = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ].slice(0, base)
+
+  const res = num.split('').every((el) => digits.includes(el))
+
+  return res
+}
+console.log(validateBase('7623', 8))
+console.log(validateBase('ABCDEF', 16))
