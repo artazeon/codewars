7a4f295d0184c77ab8b12010a131fc99a0556d51
artazeon <artazeon@gmail.com>
2026-05-15 22:05:26 +0300

    7ky

---
+++ 7ky/8 towers copy.js
@@ -0,0 +1,10 @@
+function towerCombination(n) {
+  let result = 1n
+  for (let i = 2n; i <= BigInt(n); i++) {
+    result *= i
+  }
+  return result
+}
+
+console.log(towerCombination(3))
+console.log(towerCombination(18))
