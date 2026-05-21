85d0285a39e39512c8fa08f8d1b142060df78628
artazeon <artazeon@gmail.com>
2026-05-21 20:39:47 +0300

    7ky

---
+++ 7ky/8 towers copy.js
@@ -0,0 +1,21 @@
+function rotateMatrix(matrix) {
+  const newMatrix = []
+  const row = matrix[0].length
+  const col = matrix.length
+
+  for (let i = 0; i < row; i++) {}
+
+  return col
+}
+
+console.log(
+  rotateMatrix([
+    [-1, 4, 5],
+    [2, 3, 4],
+  ]),
+  [
+    [5, 4],
+    [4, 3],
+    [-1, 2],
+  ],
+)
