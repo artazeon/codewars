a2c76cbc248d973073a47e45e30de560ebf85a71
artazeon <artazeon@gmail.com>
2026-05-26 23:29:55 +0300

    7ky

---
+++ 7ky/Is Sator Square.js
@@ -0,0 +1,13 @@
+function isSatorSquare(tablet) {
+  //  <----  hajime!
+
+  return true || false
+}
+
+console.log(
+  isSatorSquare([
+    ['T', 'E', 'N'],
+    ['E', 'Y', 'E'],
+    ['N', 'E', 'T'],
+  ]),
+)
