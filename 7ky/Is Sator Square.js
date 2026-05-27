505a2158bff852782a28e1ab4d735384842ee3d2
artazeon <artazeon@gmail.com>
2026-05-27 12:21:29 +0300

    7ky

--- 7ky/Is Sator Square.js
+++ 7ky/Is Sator Square.js
@@ -1,13 +1,42 @@
 function isSatorSquare(tablet) {
-  //  <----  hajime!
+  const n = tablet.length
 
-  return true || false
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (
+        tablet[i][j] !== tablet[j][i] ||
+        tablet[i][j] !== tablet[n - 1 - i][n - 1 - j]
+      ) {
+        return false
+      }
+    }
+  }
+
+  return true
 }
 
 console.log(
   isSatorSquare([
-    ['T', 'E', 'N'],
-    ['E', 'Y', 'E'],
-    ['N', 'E', 'T'],
+    ['B', 'A', 'T', 'S'],
+    ['A', 'B', 'U', 'T'],
+    ['T', 'U', 'B', 'A'],
+    ['S', 'T', 'A', 'B'],
+  ]),
+)
+console.log(
+  isSatorSquare([
+    ['S', 'A', 'T', 'O', 'R'],
+    ['A', 'R', 'E', 'P', 'O'],
+    ['T', 'E', 'N', 'E', 'T'],
+    ['O', 'P', 'E', 'R', 'A'],
+    ['R', 'O', 'T', 'A', 'S'],
+  ]),
+)
+console.log(
+  isSatorSquare([
+    ['B', 'A', 'T', 'S'],
+    ['U', 'B', 'U', 'T'],
+    ['T', 'U', 'B', 'U'],
+    ['S', 'T', 'A', 'B'],
   ]),
 )
