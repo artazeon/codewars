785f374a0737e84268362a5b4f7cead6fefd70a3
artazeon <artazeon@gmail.com>
2026-07-26 22:12:02 +0300

    7ky

---
+++ 7ky/8 towers copy.js
@@ -0,0 +1,11 @@
+function mergeStrings(first, second) {
+  let result = ''
+  let result2 = ''
+  for (let i = 0; i < first.length; i++) {
+    for (let j = 0; j < second.length; j++) {}
+  }
+
+  return result + result2
+}
+
+console.log(mergeStrings('abcde', 'cdefgh'))
