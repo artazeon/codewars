06e5b2482c5de508a1ff7251d625e2571d9fde57
artazeon <artazeon@gmail.com>
2026-08-24 20:17:42 +0300

    7ky

--- 7ky/8 towers copy.js
+++ 7ky/8 towers copy.js
@@ -1,7 +1,3 @@
 function digitsAverage(input) {}
 
 console.log(digitsAverage(246))
-console.log(digitsAverage(246))
-console.log(digitsAverage(246))
-console.log(digitsAverage(246))
-console.log(digitsAverage(246))
