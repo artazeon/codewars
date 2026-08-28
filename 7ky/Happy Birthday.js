0c14da5b14b01ab5a0722b30e9089aab4bd94a6c
artazeon <artazeon@gmail.com>
2026-08-28 15:21:18 +0300

    7ky

--- 7ky/Happy Birthday.js
+++ 7ky/Happy Birthday.js
@@ -1,6 +1,5 @@
 function wrap(height, width, length) {
-  //your code here
-  return 0
+  return 2 * (height + width + length + Math.min(height, width, length)) + 20
 }
 
 console.log((wrap(17, 32, 11), 162))
